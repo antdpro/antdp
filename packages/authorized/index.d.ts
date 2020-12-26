8bc7fffc7007f05632b9da0a17be9b2c17bc1811
SunLu <1011771396@qq.com>
2020-12-26 16:56:25 +0800

    feat:添加参数说明

--- packages/authorized/index.d.ts
+++ packages/authorized/index.d.ts
@@ -11,6 +11,28 @@ export interface AuthorizedProps {
   redirectPath?: string;
   children?: React.ReactNode;
 }
+/**
+ * @description: 页面权限
+ * @param {Array} menuRouter 原始 routes.json 文件中路由
+ * @param {string} path 当前路径
+ * @return {*}
+ */
+export type getAuthorizedPage = (menuRouter: Array, path: string) => boolean
+
+/**
+ * @description: 进行路由筛选，对无权限路由进行移除
+ * @param {Array} menuRouter 原始 routes.json 文件中路由
+ * @return {*}
+ */
+export type getMenuItems = (menuRouter: Array<any>) => Array<any>
+
+export interface AuthorizedBtn {
+  // 权限路径
+  path?: string,
+  // 展示内容
+  children?: React.ReactNode
+}
+
 
 declare const _default: (props?: AuthorizedProps) => React.ReactNode;
 export default _default;
--- packages/authorized/src/format.js
+++ packages/authorized/src/format.js
@@ -10,12 +10,17 @@
  * ]
  */
 export const formatMenu = (routes) => {
-    // 所有的菜单 
+    //  所有的 权限菜单 
     const authMenus = sessionStorage.getItem("authMenu")&&JSON.parse(sessionStorage.getItem("authMenu"))||[]
-    // 根据 menuUrl 判断是否存在权限
    return getMenuItem(routes,authMenus)
 }
-// 过滤路由
+/**
+ * @description:  过滤路由 
+ * @param {Array} routes 原始 routes.json 文件中路由
+ * @param {Array} authMenus 权限路由
+ * @param {Array} list 保存返回 有权限 路由
+ * @return {*}
+ */
 const getMenuItem = (routes, authMenus, list = []) => {
     (routes || []).forEach(item => {
       const { path } = item
@@ -30,7 +35,12 @@ const getMenuItem = (routes, authMenus, list = []) => {
     })
     return list
 }
-
+/**
+ * @description: 根据 menuUrl 判断是否存在权限
+ * @param {string} path 路径
+ * @param {Array} authMenus 权限路由
+ * @return {*}
+ */ 
 const checkRouter = (path,authMenus) => {
     let fig = true;
     const finx = authMenus.findIndex(item => item.menuUrl === path)
@@ -41,11 +51,11 @@ const checkRouter = (path,authMenus) => {
 }
 /**
  * @description: 按钮权限
- * @param {string} path
- * @param { ReactElement } children
+ * @param { string } path 路径
+ * @param { React.ReactNode } children 展示内容
  */
 export const FormatBtn = ({ path, children }) => {
-    if(!path){return null}
+    if(!path){return children||null}
     const authBtns =  sessionStorage.getItem("authBtn")&&JSON.parse(sessionStorage.getItem("authBtn"))||[]
     const finx = authBtns.findIndex(item => item.menuUrl === path)
     if (finx === -1) {
@@ -56,8 +66,8 @@ export const FormatBtn = ({ path, children }) => {
 /**
  * @description: 页面权限
  * authMenus 权限路由 
- * @param {Array} allRouters
- * @param {string} pathname
+ * @param {Array} allRouters 原始 routes.json 文件中路由
+ * @param {string} pathname 当前路径
  * @return {*}
  */ 
 export const getFormatPage = (allRouters,pathname) => {
@@ -69,24 +79,27 @@ export const getFormatPage = (allRouters,pathname) => {
       const allMenu = !!sessionStorage.getItem('authMenu')
         ? JSON.parse(sessionStorage.getItem('authMenu'))
         : [];
-      const check =
-        mapRouterCheck(allRouters, pathname).length > 0 ? true : false;
+      const check = mapRouterCheck(allRouters, pathname).length > 0 ? true : false;
       // 若不在权限路由中则提示无权限
-      const authCheck =
-        allMenu.findIndex((va) => va.menuUrl == pathname)>= 0
-          ? true
-          : false;
+      const authCheck = allMenu.findIndex((va) => va.menuUrl == pathname) >= 0 ? true : false;
       if (check && !authCheck) {
         // 无权访问
         return 403;
       } else if (check && authCheck) {
+        // 其他
         return true;
       } else {
         // 找不到
         return 404;
       }
 }
-
+/**
+ * @description: 
+ * @param { Array } data 路由
+ * @param { string } pathName 路径
+ * @param { Array<true|flase> } list  [ true | flase ]
+ * @return {*}
+ */
 const mapRouterCheck = (data, pathName, list = []) => {
   (data || []).forEach((val) => {
     const { path, routes } = val;

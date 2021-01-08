9c816d3e7fff6507d9d65b020245ad20070f6aae
SunLu <1011771396@qq.com>
2021-01-08 14:27:26 +0800

    fix:删除不用ts

--- packages/authorized/index.d.ts
+++ packages/authorized/index.d.ts
@@ -17,14 +17,7 @@ export interface AuthorizedProps {
  * @param {string} path 当前路径
  * @return {*}
  */
-export type getAuthorizedPage = (menuRouter: Array, path: string) => boolean
-
-/**
- * @description: 进行路由筛选，对无权限路由进行移除
- * @param {Array} menuRouter 原始 routes.json 文件中路由
- * @return {*}
- */
-export type getMenuItems = (menuRouter: Array<any>) => Array<any>
+export type getAuthorizedPage = (menuRouter: Array<any>, path: string) => boolean
 
 export interface AuthorizedBtn {
   // 权限路径

749c317d5b3875f4d0fdd0b61ab54c6c19b0b0fa
SunLxy <1011771396@qq.com>
2021-08-18 20:19:26 +0800

    fix:修改包引用路径

--- examples/antdp-base/src/pages/Home/index.jsx
+++ examples/antdp-base/src/pages/Home/index.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
-
+import { QuickForm } from '@antdp/antdp-ui';
 export default class Home extends Component {
   render() {
-    return <div>这里是首页，欢迎您！</div>;
+    return (
+      <div>
+        这里是首页，欢迎您！
+        <QuickForm
+          header="标题"
+          formDatas={[
+            {
+              type: 'input',
+              label: '测试',
+              name: 'ane',
+            },
+          ]}
+        />
+      </div>
+    );
   }
 }

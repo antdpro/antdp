d456d11016fde17874832684c1eedffb1cfe0996
WX <42308734@qq.com>
2021-08-18 23:13:41 +0800

    fix-example home bug

--- examples/antdp-base/src/pages/Home/index.jsx
+++ examples/antdp-base/src/pages/Home/index.jsx
@@ -1,28 +1,10 @@
 import React, { Component } from 'react';
-import { QuickForm } from '@antdp/antdp-ui';
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.ref = React.createRef();
   }
   render() {
-    return (
-      <QuickForm
-        ref={this.ref}
-        formDatas={[
-          {
-            label: '备注',
-            name: 'remark',
-            type: 'input',
-          },
-          {
-            label: '水果',
-            name: 'fruit',
-            type: 'select',
-            options: [{ label: 'apple', value: 1 }],
-          },
-        ]}
-      />
-    );
+    return <div>欢迎来到antdp</div>;
   }
 }

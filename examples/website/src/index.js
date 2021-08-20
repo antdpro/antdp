5745221a6bc394fc07e6a4084f3f8aa9c4e2871c
jaywcjlove <398188662@qq.com>
2021-08-21 00:25:40 +0800

    website: update menu.

--- examples/website/src/index.js
+++ examples/website/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Suspense, Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { Layout, Menu } from 'antd';
-import { GlobalOutlined, AppstoreAddOutlined } from '@ant-design/icons';
+import { Layout, Menu, Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 import GitHubCorners from '@uiw/react-github-corners';
 import {
   HashRouter as Router,
@@ -12,16 +12,7 @@ import {
 } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import Example from './pages/example';
-import BasicLayouts from './pages/basic-layouts';
-import Authorized from './pages/authorized';
-import DocumentTitle from './pages/document-title';
-import Config from './pages/config';
-import Fullscreen from './pages/fullscreen';
-import Antdp from './pages/antdp';
-import LayoutTabs from './pages/layout-tabs';
-import UserLogin from './pages/user-login';
-import PageLoading from './pages/page-loading';
+import { data } from './route';
 
 const { Sider, Content } = Layout;
 
@@ -29,36 +20,13 @@ function MenuContent() {
   let location = useLocation();
   return (
     <Menu theme="dark" mode="inline" defaultSelectedKeys={[location.pathname]}>
-      <Menu.Item key="/" icon={<AppstoreAddOutlined />}>
-        <NavLink to="/">Home</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/example" icon={<GlobalOutlined />}>
-        <NavLink to="/example">示例</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/basic-layouts" icon={<AppstoreAddOutlined />}>
-        <NavLink to="/basic-layouts">BasicLayouts</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/authorized" icon={<AppstoreAddOutlined />}>
-        <NavLink to="/authorized">Authorized</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/config" icon={<AppstoreAddOutlined />}>
-        <NavLink to="/config">Config</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/document-title" icon={<AppstoreAddOutlined />}>
-        <NavLink to="/document-title">Document Title</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/layout-tabs" icon={<AppstoreAddOutlined />}>
-        <NavLink to="/layout-tabs">Layout Tabs</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/fullscreen" icon={<AppstoreAddOutlined />}>
-        <NavLink to="/fullscreen">Fullscreen</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/user-login" icon={<AppstoreAddOutlined />}>
-        <NavLink to="/user-login">User Login</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/page-loading" icon={<AppstoreAddOutlined />}>
-        <NavLink to="/page-loading">Page Loading</NavLink>
-      </Menu.Item>
+      {data.map(({ path, name, icon }) => {
+        return (
+          <Menu.Item key={path} icon={icon}>
+            <NavLink to={path}>{name}</NavLink>
+          </Menu.Item>
+        );
+      })}
     </Menu>
   );
 }
@@ -73,43 +41,34 @@ ReactDOM.render(
     />
     <Router>
       <Layout style={{ height: '100%' }}>
-        <Sider>
+        <Sider style={{ overflow: 'auto' }}>
+          <div className="logo">antdp</div>
           <MenuContent />
         </Sider>
         <Layout>
           <Content style={{ height: '100%', overflow: 'auto' }}>
-            <Switch>
-              <Route exact path="/">
-                <Antdp />
-              </Route>
-              <Route path="/basic-layouts">
-                <BasicLayouts />
-              </Route>
-              <Route path="/authorized">
-                <Authorized />
-              </Route>
-              <Route path="/antdp">
-                <Example />
-              </Route>
-              <Route path="/config">
-                <Config />
-              </Route>
-              <Route path="/document-title">
-                <DocumentTitle />
-              </Route>
-              <Route path="/layout-tabs">
-                <LayoutTabs />
-              </Route>
-              <Route path="/fullscreen">
-                <Fullscreen />
-              </Route>
-              <Route path="/user-login">
-                <UserLogin />
-              </Route>
-              <Route path="/page-loading">
-                <PageLoading />
-              </Route>
-            </Switch>
+            <Suspense
+              fallback={
+                <Spin
+                  indicator={
+                    <LoadingOutlined
+                      style={{ fontSize: 24, margin: '20px 20px' }}
+                      spin
+                    />
+                  }
+                />
+              }
+            >
+              <Switch>
+                <Fragment>
+                  {data.map(({ name, icon, ...item }, key) => {
+                    const { path, component: Comps } = item;
+                    console.log('Comps:', item, path, Comps, data);
+                    return <Route key={key} {...item} />;
+                  })}
+                </Fragment>
+              </Switch>
+            </Suspense>
           </Content>
         </Layout>
       </Layout>

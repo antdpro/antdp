18e3d3068fb772394eb2979e259148fdcf72b889
WX <42308734@qq.com>
2021-08-21 13:52:08 +0800

    feat - 新功能(Add antdp-ui UploadGrid可拖拽上传组件

--- packages/antdp-ui/src/index.tsx
+++ packages/antdp-ui/src/index.tsx
@@ -1,3 +1,4 @@
 export { default as QuickForm } from './QuickForm';
 export { default as CardPro } from './CardPro';
 export { default as ButtonGroupPro } from './ButtonGroupPro';
+export { default as UploadGrid } from './UploadGrid';

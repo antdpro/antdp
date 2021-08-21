da71fc6641c08b4abc6f62864e617241029f14a9
jaywcjlove <398188662@qq.com>
2021-08-21 15:56:28 +0800

    webiste: update example pages.

--- examples/website/src/pages/example/index.js
+++ examples/website/src/pages/example/index.js
@@ -1,16 +1,31 @@
+import { Spin } from 'antd';
+import { Fragment, useState } from 'react';
+import styles from './index.module.css';
+
 export default function Pages() {
+  const [loading, setLoading] = useState(true);
   return (
-    <iframe
-      src="https://codesandbox.io/embed/github/antdpro/antdp/tree/master/examples/antdp-base?fontsize=14&hidenavigation=1&theme=dark&view=preview"
-      style={{
-        width: '100%',
-        height: '100%',
-        border: 0,
-        overflow: 'hidden',
-      }}
-      title="@example/antdp-base"
-      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-    />
+    <Fragment>
+      {loading && (
+        <div className={styles.loading}>
+          <Spin tip="Loading..." />
+        </div>
+      )}
+      <iframe
+        src="https://codesandbox.io/embed/github/antdpro/antdp/tree/master/examples/antdp-base?fontsize=14&hidenavigation=1&theme=dark&view=preview"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 0,
+          overflow: 'hidden',
+        }}
+        onLoad={(evn) => {
+          setLoading(false);
+        }}
+        title="@example/antdp-base"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      />
+    </Fragment>
   );
 }

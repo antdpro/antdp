2a01e320776fee32d76b160460ddfed509832b24
WX <42308734@qq.com>
2021-08-25 23:28:31 +0800

    (feat):Add FormDetail

--- packages/antdp-ui/src/index.tsx
+++ packages/antdp-ui/src/index.tsx
@@ -2,3 +2,4 @@ export { default as QuickForm } from './QuickForm';
 export { default as CardPro } from './CardPro';
 export { default as ButtonGroupPro } from './ButtonGroupPro';
 export { default as UploadGrid } from './UploadGrid';
+export { default as FormDetail } from './FormDetail';

67beb6742db7f0e423d14ce0f0edc7cfe6059235
WX <42308734@qq.com>
2021-08-26 21:49:44 +0800

    fix:Update QuickForm&FromDetail

--- packages/antdp-ui/src/UploadGrid/types.d.ts
+++ packages/antdp-ui/src/UploadGrid/types.d.ts
@@ -1,13 +1,13 @@
 import { UploadFile } from 'antd/es/upload/interface';
-import { UploadProps } from 'antd/lib/upload';
+import { UploadProps, UploadListProps } from 'antd/lib/upload';
 import { UploadChangeParam } from 'antd/lib/upload/interface';
 import { ReactNode } from 'react';
 
 export type Props = {
   onChange: (params: { fileList: UploadFile[] }) => void;
   children?: ReactNode;
-  useDragHandle?:boolean | undefined
-} & UploadProps
+  useDragHandle?: boolean | undefined
+} & UploadProps & UploadListProps
 
 type SortableParams = {
   props: Omit<Props, 'onChange'>;

6aa14b216bbb0a0f6fa6319df98c1e6059d2f1bc
jaywcjlove <398188662@qq.com>
2021-09-27 18:21:18 +0800

    doc(CardPro): 更新CardPro 组件。

--- packages/antdp-ui/src/index.tsx
+++ packages/antdp-ui/src/index.tsx
@@ -1,6 +1,16 @@
 export { default as QuickForm } from './QuickForm';
+export * from './QuickForm';
 export { default as CardPro } from './CardPro';
-export { default as ButtonGroupPro } from './ButtonGroupPro';
+export * from './CardPro';
 export { default as UploadGrid } from './UploadGrid';
+export * from './UploadGrid';
 export { default as FormDetail } from './FormDetail';
+export * from './FormDetail';
 export { default as InputCount } from './InputCount';
+export * from './InputCount';
+/**
+ * 这个组件有个严重问题。
+ * 报错：Module not found: Can't resolve '@@/core/umiExports' in 
+ * 这个组件 `@antdp/authorized` 引入了 `umi` 中的 `Redirect`
+ */
+export { default as ButtonGroupPro } from './ButtonGroupPro';

be81e8211453b4000549c901aec102e44814d196
jaywcjlove <398188662@qq.com>
2021-10-07 04:11:01 +0800

    website: Update create-antdp document.

--- examples/website/src/pages/create-antdp/index.js
+++ examples/website/src/pages/create-antdp/index.js
@@ -1,11 +1,32 @@
-import str from 'create-antdp/README.md';
-import Markdown from '../../components/markdown';
+import { Fragment, useState } from 'react';
+import { Spin } from 'antd';
+import styles from '../example/index.module.css';
 
 export default function Pages() {
+  const [loading, setLoading] = useState(true);
   return (
-    <Markdown
-      source={str}
-      editePath="https://github.com/antdpro/create-antdp/edit/main/README.md"
-    />
+    <Fragment>
+      {loading && (
+        <div className={styles.loading}>
+          <Spin tip="Loading..." />
+        </div>
+      )}
+      <iframe
+        src="https://antdpro.github.io/create-antdp"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 0,
+          overflow: 'hidden',
+        }}
+        onLoad={(evn) => {
+          setLoading(false);
+        }}
+        onError={(evn) => {
+          setLoading(false);
+        }}
+        title="@example/antdp-base"
+      />
+    </Fragment>
   );
 }

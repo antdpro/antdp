4e8613118d34c23e76d15893c50377be41e1b414
jaywcjlove <398188662@qq.com>
2021-12-02 11:37:50 +0800

    chore(website): fix website build script.

--- examples/website/.kktrc.js
+++ examples/website/.kktrc.js
@@ -4,6 +4,7 @@ import lessModules from '@kkt/less-modules';
 export default (conf, env, options) => {
   conf = lessModules(conf, env, options);
   conf = rawModules(conf, env, options);
+  conf.module.exprContextCritical = false;
   conf.output = { ...conf.output, publicPath: './' };
   return conf;
 };

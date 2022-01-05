af49a69cc4f863fef844ffe1b7fdb8ad94e35d38
jaywcjlove <398188662@qq.com>
2022-01-05 10:22:09 +0800

    chore(deps): update dependency kkt to v7.
    Upgrade react-scripts to v5, Support Webpack 5.x
    https://github.com/kktjs/kkt/issues/198

--- examples/website/.kktrc.js
+++ examples/website/.kktrc.js
@@ -5,6 +5,30 @@ export default (conf, env, options) => {
   conf = lessModules(conf, env, options);
   conf = rawModules(conf, env, options);
   conf.module.exprContextCritical = false;
-  conf.output = { ...conf.output, publicPath: './' };
+  if (env === 'production') {
+    conf.output = { ...conf.output, publicPath: './' };
+    conf.optimization = {
+      ...conf.optimization,
+      splitChunks: {
+        cacheGroups: {
+          reactvendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendor',
+            chunks: 'all',
+          },
+          refractor: {
+            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+            name: 'refractor-vendor',
+            chunks: 'all',
+          },
+          antd: {
+            test: /[\\/]node_modules[\\/](antd)[\\/]/,
+            name: 'antd-vendor',
+            chunks: 'all',
+          },
+        },
+      },
+    };
+  }
   return conf;
 };
